b0ff1da3a3b675fe2f5756deac1ac482ea0d5751
Magdalena Bal <168341156+MagdaBal@users.noreply.github.com>
2025-01-14 17:56:37 +0100

    Add files via upload
    Models added

---
+++ Models/Login.cs
@@ -0,0 +1,11 @@
+﻿namespace NewsPlatform3.Models
+{
+    public class Login
+    {
+        public Boolean isL { get; set; }
+        public String username { get; set; }        
+        public String level { get; set; }    
+        public Guid Guid { get; set; }
+
+    }
+}
